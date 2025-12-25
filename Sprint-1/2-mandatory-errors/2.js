00f53022b201989f883de7d8fbede612a8d5b51c
Gbemisola <fadeyi.victoria@yahoo.com>
2025-12-25 18:11:41 +0100

    i declared referenceError

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,12 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
+// console.log(`I was born in ${cityOfBirth}`);
+// const cityOfBirth = "Bolton";
+
+// The error here is referenceError
+// To fix this, the declaration will have to move to the top
+
 const cityOfBirth = "Bolton";
+
+console.log(`I was born in ${cityOfBirth}`);
