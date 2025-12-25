f79c6d1f9a0277e24783b148c171a6768f038b6b
Gbemisola <fadeyi.victoria@yahoo.com>
2025-12-25 18:07:53 +0100

    i reassigned values

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,10 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
-age = age + 1;
+// const age = 33;
+// age = age + 1;
+
+// To do this i will change "const" to "let"
+let age = 33;
+age = age + 1; // This value is now a valid reassignment
+
+console.log(age)
