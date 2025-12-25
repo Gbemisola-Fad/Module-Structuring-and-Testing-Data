184cf73299d45890775a00e25c459050900535d2
Gbemisola <fadeyi.victoria@yahoo.com>
2025-12-25 18:14:22 +0100

    how i solve problems

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,6 +1,5 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't // This is just an instruction for the first activity - but it is just for human consumption
+// This is just an instruction for the first activity - but it is just for human consumption
 // We don't want the computer to run these 2 lines - how can we solve this problem?
 
 // this text is not a valid javascript syntax, which could cause a syntaxError
-// to fix this, a single-line comment marker (//) should be at the beginning of each line.want the computer to run these 2 lines - how can we solve this problem?
+// to fix this, a single-line comment marker (//) should be at the beginning of each line.
