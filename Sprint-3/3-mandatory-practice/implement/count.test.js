ce3b1a6c7017d34dd2ec6e29ff7cf6dc6e1c20fe
Gbemisola <fadeyi.victoria@yahoo.com>
2026-01-10 15:30:42 +0100

    I provided a required countchar

--- Sprint-3/3-mandatory-practice/implement/count.test.js
+++ Sprint-3/3-mandatory-practice/implement/count.test.js
@@ -22,3 +22,4 @@ test("should count multiple occurrences of a character", () => {
 // And a character char that does not exist within the case-sensitive str,
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of the char were found in the case-sensitive str.
+
