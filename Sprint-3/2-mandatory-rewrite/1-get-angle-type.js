42de7a6dba09e9794d63c88ebce8ca9c594c2b55
Gbemisola <fadeyi.victoria@yahoo.com>
2026-01-28 10:04:00 +0100

    implement getOrdinalNumber, Repeat, PasswordValidator

--- Sprint-3/2-mandatory-rewrite/1-get-angle-type.js
+++ Sprint-3/2-mandatory-rewrite/1-get-angle-type.js
@@ -1,6 +1,16 @@
 function getAngleType(angle) {
   if (angle === 90) return "Right angle";
-  // replace with your completed function from key-implement
+  if (angle < 90) return "Acute angle";
+  if (angle > 90 && angle < 180) return "Obtuse angle";
+  if (angle === 180) return "Straight angle";
+  if (angle > 180 && angle < 360) return "Reflex angle";
+}
+
+
+
+
+
+
 
 
 // Don't get bogged down in this detail
@@ -8,5 +18,3 @@ function getAngleType(angle) {
 // We will upgrade our approach to ES6 modules in the next course module, so for now 
 // we have just written the CommonJS module.exports syntax for you
 module.exports = getAngleType;
-
-
--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -1,23 +1,11 @@
-const isProperFraction = require("./2-is-proper-fraction");
-
-test("should return true for a proper fraction", () => {
-  expect(isProperFraction(2, 3)).toEqual(true);
-});
-
-// Case 2: Identify Improper Fractions: 
-test("should return false for an improper fraction", () => {
-  expect(isProperFraction(5, 4)).toEqual(false);
-}); 
-
-
-
-// Case 3: Identify Negative Fractions:
-test("should return false for a negative fraction", () => {
-  expect(isProperFraction(-2, 3)).toEqual(true);
-});
-
-
-// Case 4: Identify Equal Numerator and Denominator:
-test("should return false when numerator equals denominator", () => {
-  expect(isProperFraction(4, 4)).toEqual(false);
-});
+function getCardValue(card) {
+    const rank = card.slice(0, -1); // Extract rank by removing the last character (suit)
+    if (rank === "A") return 11;
+    if (["K", "Q", "J", "10"].includes(rank)) return 10;
+    const numericValue = parseInt(rank, 10);
+    if (numericValue >= 2 && numericValue <= 9) return numericValue;
+   
+    throw new Error("Invalid card rank.");  
+    }
+    
+module.exports = getCardValue;
